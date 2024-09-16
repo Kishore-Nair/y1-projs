db3c0ff73f69d6236ddcad303737e149928f8a9b
kishore-learner <kishorenair001@gmail.com>
2024-09-16 22:51:14 +0530

    errors rectified

--- tic tac toe.py
+++ tic tac toe.py
@@ -21,10 +21,10 @@ def checkWin(xState, zState):
     wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     for win in wins:
         if sum(xState[win[0]], xState[win[1]], xState[win[2]]) == 3:
-            print("\nX wong")
+            print("\nX won\n")
             return 1
         elif sum(zState[win[0]], zState[win[1]], zState[win[2]]) == 3:
-            print("\nO won")
+            print("\nO won\n")
             return 0
         
     return -1
